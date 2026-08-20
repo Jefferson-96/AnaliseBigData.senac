08d7c6285ee30b578bb5e3ea31f1fd968e25dca3
JEFFERSON RODRIGUES MOTA <jefferson.mota@aluno.senac.rio>
2026-08-19 21:50:57 -0300

    aula17-08.py

---
+++ AULAS/semana-02/aula19-08.py
@@ -0,0 +1,63 @@
+# nome = input ("informe seu nome")
+
+# if name=="jefferson":
+#     resposta="jefferson presente!"
+# elif nome== "Phellipe presente!":
+#     resposta= "Phellipe presente!"
+
+
+#VISÃO IF/ELSE:
+
+# mes = int(input("Informe o mês de seu nascimento:"))
+
+# if mes==1:
+#     signo="Aquário"
+# elif mes==2:
+#     signo="Peixes"
+# elif mes==3:
+#     signo="Áries"
+# elif mes==4:
+#     signo="Touro"
+# elif mes==5:
+#     signo="Gêmeos"
+# elif mes==6:
+#     signo="Câncer"
+# elif mes==7:
+#     signo="Leão"
+# elif mes==8:
+#     signo="Virgem"
+# elif mes==9:
+#     signo="Libra"
+# elif mes==10:
+#     signo="Escorpião"
+# elif mes==11:
+#     signo="Sagitário"
+# else:
+#     signo="Capricórnio"
+
+# print(f"Seu signo é {signo}.")
+
+
+#VISÃO Match/Case
+
+# match mes:
+#     case 1:
+#         signo="Aquário"
+#     case 2:
+#         signo="Áries"
+#     case 3:
+#         signo="Touro"
+#     case 4:
+#         signo="Gêmeos"
+#     case 5:
+#         signo="Câncer"
+#     case _:
+#         signo="Número de mês inválido"
+
+# print(f"{signo}.")
+
+#DEMONSTRAÇÃO
+
+# meunome = "jefferson"
+# for i in meunome:
+#     print(i)
---
+++ AULAS/semana-02/exercicios.py
@@ -0,0 +1,39 @@
+#ATIVIDADES PRATICAS
+
+#1 - Cálculo de Lâmpadas:
+
+potencia = float(input("3 watts:"))
+metro_quadrado = float(input("3m²:"))
+dimensoes = lfoat(input(3))
+
+
+
+
+
+#4 - Código de Origem do Produto:
+
+# codigo = int(input("Código de Origem do Produto:"))
+# match codigo:
+#     case 1:
+#         codigo="Sul"
+#     case 2:
+#         codigo="Norte"
+#     case 3:
+#         codigo="Leste"
+#     case 4:
+#         codigo="Oeste"
+#     case 5 | 6:
+#         codigo="Nordeste"
+#     case 7 | 8 | 9:
+#         codigo="Sudeste"
+#     case 10:
+#         codigo="Centro-Oeste"
+#     case 11:
+#         codigo="Noroeste" 
+#     case _:
+#         codigo="Importado"            
+
+# print(f"região:{codigo}.")
+
+
+
