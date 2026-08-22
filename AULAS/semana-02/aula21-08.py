d4c7bf8899decf20c702b06428661688fcb4ffaa
JEFFERSON RODRIGUES MOTA <jefferson.mota@aluno.senac.rio>
2026-08-21 21:25:59 -0300

    aula19-08.py

---
+++ AULAS/semana-02/aula21-08.py
@@ -0,0 +1,70 @@
+
+
+# meunome = "Jefferson"
+
+# for i in meunome:
+    # print (i)
+#    print (meunome[2])
+
+# for i in range(10):
+#     print (i)
+
+# for i in range(-10):
+#     print (i)
+
+# for i in range(1,10):
+#     print (i)
+
+# for i in range(2,101,2):
+#     print (i)
+#o comando a cima serve para contar de 2 em 2, ou numeros pares
+
+# for i in range(-101,-1,2):
+#     print (i)
+# o comando a cima serve para contagem regressiva
+
+
+#WHILE
+
+# somador = int(input("Registro:"))
+# controle = 0
+
+# while controle <= 30:
+#     controle=controle+somador
+#     somador = int(input("registro:"))
+
+# print("oficina lotada")    
+
+
+#FOR 
+
+# acertou = 0
+# while acertou < 5:
+#     print(f"Número {acertou+1} de 5:")
+#     um = float(input("Digite um número:"))
+
+# dobro = num * 2
+# triplo = num * 3
+# quádruplo = num * 4
+
+# print(f"resultado; Dobro={dobro}, Triplo{triplo}, Quádruplo{quádruplo}\n")
+# acertou+=1
+
+#DO WHILE
+
+contador = 0 # Inicializamos o contador
+limite = 5 # Definimos o limite
+while contador < limite: # A condição de parada: Enquanto o contador for menor que 
+    print(f"Número {contador + 1} de {limite}:")
+    num = float(input("Digite um número: "))
+ 
+dobro = num * 2
+triplo = num * 3
+quádruplo = num * 4
+ 
+print(f" Resultado: Dobro={dobro}, Triplo={triplo}, Quádruplo={quádruplo}\n")
+
+
+
+
+
--- AULAS/semana-02/exercicios.py
+++ AULAS/semana-02/exercicios.py
@@ -2,9 +2,52 @@
 
 #1 - Cálculo de Lâmpadas:
 
-potencia = float(input("3 watts:"))
-metro_quadrado = float(input("3m²:"))
-dimensoes = lfoat(input(3))
+# potencia = float(input("Digite a potência da lâmpada (W): "))
+# largura = float(input("Digite a largura do cômodo (m): "))
+# comprimento = float(input("Digite o comprimento do cômodo (m): "))
+
+# area = largura * comprimento
+# potencia_necessaria = area * 3
+# lampadas = potencia_necessaria / potencia
+
+# match lampadas % 1:
+#     case 0:
+#         lampadas = int(lampadas)
+#     case _:
+#         lampadas = int(lampadas) + 1
+
+# print("Número de lâmpadas necessárias:", lampadas)
+
+
+
+
+#2 - Quantidade de Caixas de Azulejos:
+
+# comprimento = float(input("Digite o comprimento: "))
+# largura = float(input("Digite a largura: "))
+# altura = float(input("Digite a altura: "))
+
+# area = 2 * (comprimento + largura) * altura
+# caixas = area / 1.5
+
+# if caixas % 1 != 0:
+#     caixas = int(caixas) + 1
+# else:
+#     caixas = int(caixas)
+
+# print("Quantidade de caixas:", caixas) 
+
+
+
+
+
+#3. Rendimento do Taxista:
+
+
+
+
+
+
 
 
 
