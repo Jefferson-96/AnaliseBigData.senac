2296be78eb97ab4474d6b33be76297acdccb2fe5
JEFFERSON RODRIGUES MOTA <jefferson.mota@aluno.senac.rio>
2026-08-28 21:25:51 -0300

    aula2808.py

---
+++ AULAS/semana-03/aula2808.py
@@ -0,0 +1,83 @@
+#FUNÇÕES E MÓDULOS
+
+#SORTEIO DE NÚMEROS
+
+# import random
+
+# numero_random = random.randint(1,30)
+
+# print(numero_random)
+
+
+# def sorteiame():
+#     '''                                                   # <===== (aspas tripla é outra forma de comentar o texto)
+#     Algoritmo escolhe e retorna um número
+#     inteiro aleatório no intervalo de 1 até 30.
+#     '''
+#     import random
+
+#     numero_random = random.randint(1,30)
+
+#     return numero_random
+
+
+
+
+#2. CALCULADORA DE IMC
+
+
+# def calcular_imc(peso, altura):
+#     imc = peso / (altura * altura)
+#     return imc
+
+
+# def obter_classificacao(imc):
+#     if imc < 18.5:
+#         return "Abaixo do peso"
+#     elif imc < 25:
+#         return "Peso normal"
+#     elif imc < 30:
+#         return "Sobrepeso"
+#     else:
+#         return "Obesidade"
+
+
+# n = int(input("Digite a quantidade de pessoas: "))
+
+# for i in range(n):
+#     print("\nPessoa", i + 1)
+
+#     peso = float(input("Digite o peso: "))
+#     altura = float(input("Digite a altura: "))
+
+#     imc = calcular_imc(peso, altura)
+#     classificacao = obter_classificacao(imc)
+
+#     print("IMC:", round(imc, 2))
+#     print("Classificação:", classificacao)
+
+
+
+
+
+# import time
+
+# #DEFINIÇÃO DA FUNÇÃO
+# def dar_boas_vindas():
+#     print("-"*40)
+#     print(" Bem-vindo ao nosso aplicativo! 😃")
+#     print("-"*40)
+
+# 2. CHAMADA DA FUNÇÃO
+# O codigo abaixo só será executado se você "chamar" a função pelo nome
+
+
+
+
+
+
+
+
+
+
+
---
+++ AULAS/semana-03/main.py
@@ -0,0 +1,9 @@
+# from aula2808 import sorteiame
+
+# resultado = sorteiame()
+# print(resultado)
+
+from aula2808 import calcular_imc
+resultado = calcular_imc()
+print(calcular_imc)
+
