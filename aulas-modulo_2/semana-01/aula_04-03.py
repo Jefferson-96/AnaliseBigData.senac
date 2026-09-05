536eb33d802218df4f23c77d0f80c6274797eb18
JEFFERSON RODRIGUES MOTA <jefferson.mota@aluno.senac.rio>
2026-09-04 21:43:11 -0300

    aulas-modulo_2

---
+++ aulas-modulo_2/semana-01/aula_04-03.py
@@ -0,0 +1,27 @@
+
+#venv/scripts/activate
+#Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+#deactivate
+#pip install -r requirements.txt
+
+import pandas as pd
+import numpy as np 
+
+numeros_impares = [43,55,1,3,11,27,109]
+numeros_seq = [2,3,4,5,6,6,7]
+print(type(numeros_impares))
+
+serie_impares = pd.Series(numeros_impares)
+print(serie_impares)
+print(type(serie_impares))
+
+print(serie_impares.sum())
+print(serie_impares.mean())
+print(serie_impares.max())
+print(len(serie_impares))
+print(serie_impares.describe())
+print(serie_impares[serie_impares>50])
+
+
+serie2_impares = pd.Series(numeros_impares, index=['a','b','c','d','e','f','j'])
+print(serie2_impares)
